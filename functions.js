cdfaf6fce31638858db5b3c157837f25c6f1b599
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-03-29 19:47:47 +0300

    functions

---
+++ functions.js
@@ -0,0 +1,15 @@
+// given an array of strings,use a function to reverse all the elements in the string in ascending order and the specific elements in descending order
+
+
+
+
+// Using Js functions and array of numbers, return positive if an element within the array is positive ,negative if an element is negative zero
+
+
+// Given an array of objects,where each object represents an employee with an id,name,and salary property ,write a function that returns a new array of employee objects sorted by their salary in ascending order.
+
+
+
+// Write a function that accepts an array of numbers and uses the forEach() method to console.log each number multiplied by 2.
+
+// Write a function that accepts an array of numbers and uses the first four items multiplied by 8 and the last two added by 5 .console the array wuth the new values
